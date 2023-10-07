addf53b3e08fbd6575d201cdf8c87f0257c92172
Abc <bjulian127@gmail.com>
2023-10-06 19:56:50 -0500

    .
    .

--- MoneyBankService/MoneyBankService.Api/Mappers/MappingProfile.cs
+++ MoneyBankService/MoneyBankService.Api/Mappers/MappingProfile.cs
@@ -2,18 +2,25 @@
 using MoneyBankService.Api.Dto;
 using MoneyBankService.Domain.Entities;
 
-namespace MoneyBankService.Api.Mappers;
-
-public class MappingProfile : Profile
+namespace MoneyBankService.Api.Mappers
 {
-    public MappingProfile()
+    public class MappingProfile : Profile
     {
-        CreateMap<AccountDto, Account>();
-        CreateMap<Account, AccountDto>();
-        // TODO: Implement de Mapping ForMembers
-        //CreateMap<TransactionDto, Account>()
-        //    .ForMember(acc => acc.Id, opt => opt.MapFrom(trx => trx.Id));
-        //    .ForMember( ....... Oher Fields
+        public MappingProfile()
+        {
+            // Mapeo para Account
+            CreateMap<AccountDto, Account>();
+            CreateMap<Account, AccountDto>();
 
+            // Mapeo para Transaction
+            CreateMap<TransactionDto, Transaction>()
+                .ForMember(trx => trx.AccountNumber, opt => opt.MapFrom(dto => dto.AccountNumber))
+                .ForMember(trx => trx.ValueAmount, opt => opt.MapFrom(dto => dto.ValueAmount));
+            CreateMap<Transaction, TransactionDto>()
+                .ForMember(dto => dto.AccountNumber, opt => opt.MapFrom(trx => trx.AccountNumber))
+                .ForMember(dto => dto.ValueAmount, opt => opt.MapFrom(trx => trx.ValueAmount));
+
+            // Si tienes otros modelos y DTOs, puedes agregar más mapeos aquí.
+        }
     }
 }
--- MoneyBankService/MoneyBankService.Domain/Entities/Account.cs
+++ MoneyBankService/MoneyBankService.Domain/Entities/Account.cs
@@ -4,5 +4,11 @@ namespace MoneyBankService.Domain.Entities
 {
     public class Account : EntityBase
     {
+        public char AccountType { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string AccountNumber { get; set; }
+        public string OwnerName { get; set; }
+        public decimal BalanceAmount { get; set; }
+        public decimal OverdraftAmount { get; set; }
     }
 }
